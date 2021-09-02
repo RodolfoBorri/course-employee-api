344a6fdc2ae8413cbab8bf77897a2c9fb126376d
Vinícius Lourenço <vinicius.loureno@eurekalabs.com.br>
2021-09-02 03:29:07 -0300

    changed application.properties, cors config and assincrono1application

--- assincrono1/src/main/java/com/uem/assincrono1/assincrono1/Assincrono1Application.java
+++ assincrono1/src/main/java/com/uem/assincrono1/assincrono1/Assincrono1Application.java
@@ -2,6 +2,7 @@ package com.uem.assincrono1.assincrono1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Assincrono1Application {
